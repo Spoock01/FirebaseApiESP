8ad381b2acc82aa9753e48851f67e1f126201e88
João Vinícius <joaovinicius@lavid.ufpb.br>
2019-08-18 17:04:34 -0300

    Port

--- index.js
+++ index.js
@@ -81,8 +81,8 @@ app.post('/register/:roomName/:mac', (req, res) => {
 });
 
 
-app.listen(3000, function(){
-    console.log(`Server started on port 3000.`);
+app.listen(3001, function(){
+    console.log(`Server started on port 3001.`);
 });
 
 function writeUserData(roomName, macAddress) {
