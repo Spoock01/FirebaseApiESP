71944503be49e9177538a1c27a8aea1beeb8fb44
João Vinícius <joaovinicius@lavid.ufpb.br>
2019-08-18 20:31:13 -0300

    Add register validation

--- FirebaseConnection.js
+++ FirebaseConnection.js
@@ -2,62 +2,80 @@ import firebase from 'firebase';
 
 const inputEmail = 'abc@gmail.com';
 const inputPassword = 'minduin';
-var lista = [];
+var espList = [];
+var registeredList = [];
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBmsCvaUtFUSmIGIr2uZWl_6yQ5whAHMsQ",
-    authDomain: "sistemasembarcados20191-64dd3.firebaseapp.com",
-    databaseURL: "https://sistemasembarcados20191-64dd3.firebaseio.com",
-    projectId: "sistemasembarcados20191-64dd3",
-    storageBucket: "sistemasembarcados20191-64dd3.appspot.com",
-    messagingSenderId: "93476806039",
+	apiKey: "AIzaSyBmsCvaUtFUSmIGIr2uZWl_6yQ5whAHMsQ",
+	authDomain: "sistemasembarcados20191-64dd3.firebaseapp.com",
+	databaseURL: "https://sistemasembarcados20191-64dd3.firebaseio.com",
+	projectId: "sistemasembarcados20191-64dd3",
+	storageBucket: "sistemasembarcados20191-64dd3.appspot.com",
+	messagingSenderId: "93476806039",
 };
 
 firebase.initializeApp(firebaseConfig);
 
+const database = firebase.database();
+var redeRef = database.ref('/');
+
 firebase.auth().signInWithEmailAndPassword(inputEmail, inputPassword).then(function (result) {
-    console.log("User connected.");
-    redeRef.on('value', gotData, errData);
-    return;
+	console.log("User connected.");
+	redeRef.on('value', gotData, errData);
+	return;
 }).catch(function (error) {
-    var errorCode = error.code;
-    var errorMessage = error.message;
-    console.log('Err code: ', errorCode);
-    console.log('Err Message: ', listaerrorMessage);
+	var errorCode = error.code;
+	var errorMessage = error.message;
+	console.log('Err code: ', errorCode);
+	console.log('Err Message: ', listaerrorMessage);
 });
 
 
-const database = firebase.database();
-var redeRef = database.ref('Rede');
+function readDatabase(key, list, redes) {
 
-function gotData(data){
-    var redes = data.val();
+	var objKeys = Object.keys(redes[key]);
 
-    try{
+	objKeys.forEach((roomName, macAddress) => {
+		list.push(redes[key][roomName]);
+	})
 
-        lista = [];
-
-        var keys = Object.keys(redes);
-
-        for(var i = 0; i < keys.length; i++){
-            var k = keys[i];
-            if(redes[k] !== undefined)    
-                lista.push(redes[k]);       
-            else
-                console.log('Undefined object received.');   
-        }
-        
-    }catch(err){
-        console.log('Erro no try: ', err);
-    }
 }
 
-function errData(err){
-    console.log('Erro: ' + err);
+
+function gotData(data) {
+	var redes = data.val();
+
+	try {
+
+		espList = [];
+
+		var keys = Object.keys(redes);
+
+		readDatabase("Rede", espList, redes);
+		readDatabase("Registered", registeredList, redes);
+
+		// console.log("EspList: ", espList);
+		// console.log("registeredList: ", registeredList);
+
+
+	} catch (err) {
+		console.log('Erro no try... ');
+		espList = []
+		registeredList = []
+	}
 }
 
-function getRoomList() {
-    return lista;
+function errData(err) {
+	console.log('Erro: ' + err);
 }
 
-export { getRoomList, database };
+
+function getEspList() {
+	return espList;
+}
+
+function getRegisteredList() {
+	return registeredList;
+}
+
+export { getEspList, getRegisteredList, database };
--- index.js
+++ index.js
@@ -1,97 +1,124 @@
 import express from 'express';
-import {getRoomList, database} from './FirebaseConnection';
+import { getRegisteredList, getEspList, database } from './FirebaseConnection';
 
 const app = express();
 
 const registeredRooms = [{
-    name: 'Sala Synchro',
-    macAddress: 'B0:BE:76:F8:98:3F' 
+	name: 'Sala Synchro',
+	macAddress: 'B0:BE:76:F8:98:3F'
 }, {
-    name: 'Sala ConductorLab',
-    macAddress: '30:B5:C2:6D:33:B6'
+	name: 'Sala ConductorLab',
+	macAddress: '30:B5:C2:6D:33:B6'
 }, {
-    name: 'Sala CAEDC',
-    macAddress: 'C8:E7:D8:9A:C2:AC'
+	name: 'Sala CAEDC',
+	macAddress: 'C8:E7:D8:9A:C2:AC'
 }, {
-    name: 'Celular Arthur',
-    macAddress: '06:D6:AA:FC:7A:DB'
+	name: 'Celular Arthur',
+	macAddress: '06:D6:AA:FC:7A:DB'
 }]
 
 const getFilteredRooms = roomList => {
-    let filteredRooms = [];
-    for(let i = 0; i < registeredRooms.length; i++) {
-        for(let j = 0; j < roomList.length; j++) {
-            if(roomList[j].macAddress === registeredRooms[i].macAddress) {
-                filteredRooms.push(registeredRooms[i]);
-                break;
-            }
-        }
-    }
+	let filteredRooms = [];
+	for (let i = 0; i < registeredRooms.length; i++) {
+		for (let j = 0; j < roomList.length; j++) {
+			if (roomList[j].macAddress === registeredRooms[i].macAddress) {
+				filteredRooms.push(registeredRooms[i]);
+				break;
+			}
+		}
+	}
 
-    if(filteredRooms.length > 0)
-        return filteredRooms;
+	if (filteredRooms.length > 0)
+		return filteredRooms;
 
-    return null;
+	return null;
 }
 
 app.get("/:roomName", (req, res, next) => {
 
-    const { roomName } = req.params;
-    const roomList = getRoomList();
-    
-    if(roomList.length > 0) {
-        const result = roomList.filter(rn => rn.roomName === roomName );
-        
-        if(result.length > 0) {
-            console.log(`Room "${roomName}" has been found.`);
-            res.status(200).send(result[0].macAddress);
-        } else {
-            const responseText = `Room "${roomName}" not found.`;
-            console.log(responseText);
-            res.status(404).send(responseText);
-        }
-    } else {
-        console.log('There are no data in the database.');
-        res.status(404).send();
-    }
+	const { roomName } = req.params;
+	const roomList = getRegisteredList(getEspList);
+
+	if (roomList.length > 0) {
+		const result = roomList.filter(rn => rn.roomName === roomName);
+
+		if (result.length > 0) {
+			console.log(`Room "${roomName}" has been found.`);
+			res.status(200).send(result[0].macAddress);
+		} else {
+			const responseText = `Room "${roomName}" not found.`;
+			console.log(responseText);
+			res.status(404).send(responseText);
+		}
+	} else {
+		console.log('There are no data in the database.');
+		res.status(404).send();
+	}
 });
 
 app.get("/", (req, res) => {
 
-    const roomList = getRoomList();
-    // const roomList = getFilteredRooms(getRoomList());
-    // const message = `Room list.\n ${roomList}`;
-    console.log('Room list: \n', roomList);
-    res.status(200).json({"Room": roomList});
+	const roomList = getRegisteredList(getEspList);
+	// const roomList = getFilteredRooms(getRegisteredList( getEspList));
+	// const message = `Room list.\n ${roomList}`;
+	console.log('Room list: \n', roomList);
+	res.status(200).json({ "Room": roomList });
 
 });
 
 app.post('/login/:user/:password', (req, res) => {
 
-    res.status(200).send("Ainda não foi feito.");
+	res.status(200).send("Ainda não foi feito.");
 
 });
 
-app.post('/register/:roomName/:mac', (req, res) => {
+app.post('/register', (req, res) => {
 
-    writeUserData(req.params.roomName, req.params.mac);
-    res.status(200).json({
-        "status": "OK"
-    });
+	console.log(req.body)
+
+	var status = writeUserData(req.body.roomName, req.body.mac);
+
+	res.status(200).json(status);
 });
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT || 3002
 
-app.listen(PORT, function(){
-    console.log(`Server started on port 3001.`);
+app.listen(PORT, function () {
+	console.log(`Server started on port ${PORT}.`);
 });
 
 function writeUserData(roomName, macAddress) {
-    const ref = database.ref('Registered');
-    var newRoom = ref.push();
 
-    newRoom.set({
-      "roomName": roomName,
-      "macAddress": macAddress
-    });
+	var espList = getEspList();
+	var registeredList = getRegisteredList();
+
+	const exists = espList.find((obj) => {
+		return obj.macAddress === macAddress;
+	})
+
+
+	if (exists === undefined){
+		return {"status": "Mac Address not found!"};
+	}else{
+
+		const isDuplicate = registeredList.find((obj) => {
+			console.log(`Comparing ${obj.macAddress} ${macAddress}`)
+			return obj.macAddress === macAddress;
+		})
+
+		if(isDuplicate !== undefined){
+			return {"status": "Mac Address already exists!"};
+		}
+		else {
+			const ref = database.ref('Registered');
+			var newRoom = ref.push();
+	
+			newRoom.set({
+				"roomName": roomName,
+				"macAddress": macAddress
+			});
+		}
+	}
+	
+	return {"status": "OK"};
 }
